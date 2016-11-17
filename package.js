e463551b5c910ba4346c26d2a2730bb5ecb90f93
Abigail Watson <awatson1978@gmail.com>
2016-11-16 18:58:56 -0600

    gagarin tests

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-binary',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'HL7 FHIR Resource - Binary',
   git: 'https://github.com/clinical-meteor/hl7-resource-binary',
   documentation: 'README.md'
