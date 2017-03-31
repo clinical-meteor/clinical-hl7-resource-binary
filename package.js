9e3e642d02471e0f032273c16ee6b6bf4a8ddd9c
Abigail Watson <awatson1978@gmail.com>
2017-03-31 17:56:57 -0500

    DTSU3 datatypes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-binary',
-  version: '1.0.5',
+  version: '1.0.6',
   summary: 'HL7 FHIR Resource - Binary',
   git: 'https://github.com/clinical-meteor/hl7-resource-binary',
   documentation: 'README.md'
@@ -17,7 +17,7 @@ Package.onUse(function (api) {
   api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.6.8');
+  api.use('clinical:hl7-resource-datatypes@3.0.0');
 
   api.addFiles('lib/hl7-resource-binary.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
